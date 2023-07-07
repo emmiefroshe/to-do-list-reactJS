44f163f72389a93204327e3e42e99c1e4174c6ce
emmie <itoyaemmanuel07@gmail.com>
2023-07-07 02:38:09 +0100

    working on header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
+import { Header } from "./components/Header"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <p>
-        Hello World!
-      </p>
-    </>
+    <Header />
   )
-}
+};
 
 export default App
--- src/components/Header/Index.jsx
+++ src/components/Header/Index.jsx
@@ -1,7 +1,11 @@
-export function header() {
-    return(
-        <header>
-            boom boom
-        </header>
-    );
-};
+
+import styles from './header.module.css';
+import { useState } from 'react';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+        <p>boom boom</p>
+    </header>
+  )
+}
